9dea6ad679240592e22cf5296f4b8b62629fa72a
JagadishBujji <jagadish00198@gmail.com>
2023-01-06 12:23:51 +0530

    added background embracing

---
+++ javascript/brochure.js
@@ -0,0 +1,6 @@
+function NewTab() {
+    window.open(
+      "https://firebasestorage.googleapis.com/v0/b/shiridi-shelters.appspot.com/o/Shiridi%20Shelters%20Pdf.pdf?alt=media&token=84fb7469-de55-41ed-9e34-6a5b692a8c9c",
+      "_blank"
+    );
+  }
